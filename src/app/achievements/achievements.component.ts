314831cc5f3c371c5555003729972cb202f6cb16
Trushenkov Dmitry <d.trushenkov@it-soft.pro>
2022-07-26 12:53:35 +0300

    Changes: added categories for achievements

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./achievements.component.scss']
 })
 export class AchievementsComponent implements OnInit {
+  public categories: Array<string>;
+  public selectedCategory: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.categories = ['Все', 'Диплом', 'Сертификат'];
+    this.selectedCategory = this.categories[0];
   }
 
+  setCategory(cat: string) {
+    this.selectedCategory =  cat;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
   constructor(private titleService: TitleService) { }
 
   ngOnInit(): void {
-    
+
   }
 
 }
